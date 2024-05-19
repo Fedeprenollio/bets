f15f89cf7a2b35212f6266ff885607a11b5e25e0
Federico <fedeprenollio@gmail.com>
2024-05-19 05:37:09 -0300

    update delete team controller

--- server/routes/team.js
+++ server/routes/team.js
@@ -26,6 +26,16 @@ teamRouter.get('/', async (req, res) => {
   }
 })
 
+// Nueva ruta para buscar equipos por nombre
+teamRouter.get('/search/:name', async (req, res) => {
+  const name = req.params.name
+  try {
+    const equipos = await Team.find({ name: { $regex: name, $options: 'i' } })
+    res.send(equipos)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
 // Ruta para obtener un equipo por su ID
 teamRouter.get('/:id', async (req, res) => {
   const id = req.params.id
@@ -40,29 +50,10 @@ teamRouter.get('/:id', async (req, res) => {
   }
 })
 
-// teamRouter.delete('/:id', async (req, res) => {
-//   try {
-//     const id = req.params.id
-//     // Buscar el equipo por su ID y eliminarlo
-//     const deletedTeam = await Team.findByIdAndDelete(id)
-//     if (!deletedTeam) {
-//       return res.status(404).json({ message: 'No se encontró el equipo para eliminar' })
-//     }
-//     res.status(200).json({ message: 'Equipo eliminado exitosamente', deletedTeam })
-//   } catch (error) {
-//     console.error('Error al eliminar el equipo:', error)
-//     res.status(500).json({ message: 'Error al eliminar el equipo' })
-//   }
-// })
 teamRouter.delete('/:id', async (req, res) => {
   try {
     const id = req.params.id
-    // Verificar si el equipo tiene algún partido asociado
-    const matchesCount = await Match.countDocuments({ $or: [{ homeTeam: id }, { awayTeam: id }] })
-    if (matchesCount > 0) {
-      return res.status(400).json({ message: 'No se puede eliminar el equipo porque tiene partidos asociados' })
-    }
-    // Si el equipo no tiene partidos asociados, proceder con la eliminación
+    // Buscar el equipo por su ID y eliminarlo
     const deletedTeam = await Team.findByIdAndDelete(id)
     if (!deletedTeam) {
       return res.status(404).json({ message: 'No se encontró el equipo para eliminar' })
@@ -73,3 +64,22 @@ teamRouter.delete('/:id', async (req, res) => {
     res.status(500).json({ message: 'Error al eliminar el equipo' })
   }
 })
+// teamRouter.delete('/:id', async (req, res) => {
+//   try {
+//     const id = req.params.id
+//     // Verificar si el equipo tiene algún partido asociado
+//     const matchesCount = await Match.countDocuments({ $or: [{ homeTeam: id }, { awayTeam: id }] })
+//     if (matchesCount > 0) {
+//       return res.status(400).json({ message: 'No se puede eliminar el equipo porque tiene partidos asociados' })
+//     }
+//     // Si el equipo no tiene partidos asociados, proceder con la eliminación
+//     const deletedTeam = await Team.findByIdAndDelete(id)
+//     if (!deletedTeam) {
+//       return res.status(404).json({ message: 'No se encontró el equipo para eliminar' })
+//     }
+//     res.status(200).json({ message: 'Equipo eliminado exitosamente', deletedTeam })
+//   } catch (error) {
+//     console.error('Error al eliminar el equipo:', error)
+//     res.status(500).json({ message: 'Error al eliminar el equipo' })
+//   }
+// })
