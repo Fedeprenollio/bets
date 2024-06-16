ad6c66c2f65794ed334e19bcda0c68c0fb3c6e4b
Federico <fedeprenollio@gmail.com>
2024-06-16 04:56:50 -0300

    new rout and controlloer for table range

--- server/routes/match.js
+++ server/routes/match.js
@@ -4,12 +4,14 @@ import { verifyToken } from '../middleware/verifyToken .js'
 
 export const matchRouter = Router()
 
+matchRouter.get('/stats', matchController.getAllTeamsStats)
+matchRouter.get('/team/:idTeam', matchController.getMatchesByTeamId)
+matchRouter.get('/statsAc/:idTeam', matchController.getTeamStats)
 matchRouter.get('/', matchController.getAllMatches)
 matchRouter.post('/', verifyToken, matchController.createMatch)
 matchRouter.put('/:id/result', verifyToken, matchController.updateMatchResult)
 matchRouter.get('/:id', matchController.getMatchById)
-matchRouter.get('/team/:idTeam', matchController.getMatchesByTeamId)
-matchRouter.get('/statsAc/:idTeam', matchController.getTeamStats)
+
 matchRouter.get('/team-stats/:seasonId', matchController.getTeamStatsForSeason)
 
 matchRouter.delete('/:id', matchController.deleteMatchById)
